cab781bbdc030ab0640c9b6b69f6948bdc9a9cca
phisonk <phisonk@hotmail.com>
2018-09-04 23:54:29 +0700

    AJAX

--- Assignment2_chapter4/mytweets/tweets/admin.py
+++ Assignment2_chapter4/mytweets/tweets/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from tweets.models import Tweet
 
 # Register your models here.
-admin.site.register(Tweet) 
+admin.site.register(Tweet)
---
+++ Lab4_chapter5-6/django_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\elements of software construction\elements_of_software_construction\assignment2_chapter4\django_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
